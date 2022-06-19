72bcb4a814a87d252f207eddf34b400791dd517a
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-19 15:31:53 +0530

    Add files via upload

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,7 +1,7 @@
 #pip3 install -U pyrogram
 git clone https://github.com/https-github-com-rooted-cyber/Mirror-Bot-ha
 cd M*t*ha
-wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cnhma
+wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cnhmab
 wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
 ls
 chmod 777 -R *
