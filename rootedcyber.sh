e683a881aee3653e4b0110e945508d2b5a043f2d
Maruf <50019150+rooted-cyber@users.noreply.github.com>
2022-06-19 15:45:07 +0530

    Update rootedcyber.sh

--- rootedcyber.sh
+++ rootedcyber.sh
@@ -1,6 +1,8 @@
 #pip3 install -U pyrogram
-git clone https://github.com/https-github-com-rooted-cyber/Mirror-Bot-ha
-cd M*t*ha
+#git clone https://github.com/https-github-com-rooted-cyber/Mirror-Bot-ha
+#cd M*t*ha
+git clone https://github.com/harshpreets63/Mirror-Bot
+cd M*B*t
 wget -O config.env https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/cnhmab
 wget -O token.pickle https://raw.githubusercontent.com/rooted-cyber/terminal-bot/main/tk
 ls
